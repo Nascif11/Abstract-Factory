ac591e9fb462a313f229120b67c8c14da33430f4
Lucas Nascif <lucasnascif04@gmail.com>
2026-03-23 18:47:57 -0300

    Memoria desktop e laptop.

---
+++ AbstractFactory/AbstractFactory/src/main/java/com/example/AbstractFactory/MemoriaDesktop.java
@@ -0,0 +1,8 @@
+package com.example.AbstractFactory;
+
+public class MemoriaDesktop implements Memoria {
+    @Override
+    public String especificar() {
+        return "RAM 64GB DDR5 (capacidade total)";
+    }
+}
---
+++ AbstractFactory/AbstractFactory/src/main/java/com/example/AbstractFactory/MemoriaLaptop.java
@@ -0,0 +1,8 @@
+package com.example.AbstractFactory;
+
+public class MemoriaLaptop implements Memoria {
+    @Override
+    public String especificar() {
+        return "RAM 16GB LPDDR5 (otimizada para portabilidade)";
+    }
+}
